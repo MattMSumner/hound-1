679e6bf9b8f42b50d09b9c9a9e2d9a55ad4d8c34
Greg Lazarev <greg@thoughtbot.com>
2017-04-14 17:34:55 -0700

    Find the token that can reach the repo
    Also, rescue `Octokit::Unauthorized` when checking if config is
    reachable, because when the token was removed on GitHub's side that is
    the error that will be raised (i.e. 401 - Bad credentials).
    
    In `GithubApi` make `client` method private, since we shouldn't be
    calling it directly -- defeats the purpose of our wrapper.

--- app/models/user_token.rb
+++ app/models/user_token.rb
@@ -6,12 +6,18 @@ class UserToken
   end
 
   def user
-    @user ||= users_with_token.sample || user_with_default_token
+    @_user ||= users_with_token.shuffle.detect(-> { hound_user }) do |user|
+      can_reach_repository?(user)
+    end
   end
 
   private
 
-  def user_with_default_token
+  def can_reach_repository?(user)
+    GithubApi.new(user.token).repository?(repo.name)
+  end
+
+  def hound_user
     User.new(token: Hound::GITHUB_TOKEN)
   end
 
